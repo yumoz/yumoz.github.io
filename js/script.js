934f2a15c8c1a03a19c88d24102d31b67c1e2a5c
yumoz <zealouszxl@163.com>
2026-04-12 23:37:32 +0800

    feat: 重构博客主题为现代极简风格
    重构了整个博客的前端界面，包括HTML、CSS和JavaScript，采用现代极简设计风格
    - 更新了整体布局和配色方案
    - 改进了响应式设计以适应不同设备
    - 优化了搜索功能和分享功能
    - 添加了平滑滚动和动画效果
    - 改进了代码高亮显示
    - 增强了移动端体验

--- js/script.js
+++ js/script.js
@@ -1,138 +1,209 @@
-(function($){
-  // Search
-  var $searchWrap = $('#search-form-wrap'),
-    isSearchAnim = false,
-    searchAnimDuration = 200;
+/**
+ * Modern Minimalist Blog - JavaScript
+ * Clean and simple interactions
+ */
 
-  var startSearchAnim = function(){
-    isSearchAnim = true;
-  };
+(function($) {
+  'use strict';
 
-  var stopSearchAnim = function(callback){
-    setTimeout(function(){
-      isSearchAnim = false;
-      callback && callback();
-    }, searchAnimDuration);
-  };
+  // ========================================
+  // Search Functionality
+  // ========================================
+  var $searchWrap = $('#search-form-wrap');
+  var $searchInput = $('.search-form-input');
 
-  $('#nav-search-btn').on('click', function(){
-    if (isSearchAnim) return;
-
-    startSearchAnim();
+  // Open search
+  $('#nav-search-btn').on('click', function(e) {
+    e.preventDefault();
     $searchWrap.addClass('on');
-    stopSearchAnim(function(){
-      $('.search-form-input').focus();
-    });
+    setTimeout(function() {
+      $searchInput.focus();
+    }, 100);
   });
 
-  $('.search-form-input').on('blur', function(){
-    startSearchAnim();
-    $searchWrap.removeClass('on');
-    stopSearchAnim();
+  // Close search on backdrop click
+  $searchWrap.on('click', function(e) {
+    if (e.target === this) {
+      $searchWrap.removeClass('on');
+    }
   });
 
-  // Share
-  $('body').on('click', function(){
+  // Close search on Escape key
+  $(document).on('keydown', function(e) {
+    if (e.key === 'Escape' && $searchWrap.hasClass('on')) {
+      $searchWrap.removeClass('on');
+    }
+  });
+
+  // ========================================
+  // Share Functionality
+  // ========================================
+  $('body').on('click', function() {
     $('.article-share-box.on').removeClass('on');
-  }).on('click', '.article-share-link', function(e){
+  });
+
+  $('body').on('click', '.article-share-link', function(e) {
     e.stopPropagation();
 
-    var $this = $(this),
-      url = $this.attr('data-url'),
-      encodedUrl = encodeURIComponent(url),
-      id = 'article-share-box-' + $this.attr('data-id'),
-      title = $this.attr('data-title'),
-      offset = $this.offset();
+    var $this = $(this);
+    var url = $this.attr('data-url');
+    var encodedUrl = encodeURIComponent(url);
+    var id = 'article-share-box-' + $this.attr('data-id');
+    var title = $this.attr('data-title');
+    var offset = $this.offset();
 
-    if ($('#' + id).length){
-      var box = $('#' + id);
+    // Remove existing share boxes
+    $('.article-share-box.on').removeClass('on');
 
-      if (box.hasClass('on')){
-        box.removeClass('on');
-        return;
-      }
-    } else {
+    // Create or show share box
+    var $box = $('#' + id);
+    if (!$box.length) {
       var html = [
         '<div id="' + id + '" class="article-share-box">',
-          '<input class="article-share-input" value="' + url + '">',
+          '<input class="article-share-input" value="' + url + '" readonly>',
           '<div class="article-share-links">',
-            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter"></a>',
-            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook"></a>',
-            '<a href="http://pinterest.com/pin/create/button/?url=' + encodedUrl + '" class="article-share-pinterest" target="_blank" title="Pinterest"></a>',
-            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn"></a>',
+            '<a href="https://twitter.com/intent/tweet?text=' + encodeURIComponent(title) + '&url=' + encodedUrl + '" class="article-share-twitter" target="_blank" title="Twitter">',
+              '<i class="fab fa-twitter"></i>',
+            '</a>',
+            '<a href="https://www.facebook.com/sharer.php?u=' + encodedUrl + '" class="article-share-facebook" target="_blank" title="Facebook">',
+              '<i class="fab fa-facebook-f"></i>',
+            '</a>',
+            '<a href="https://www.linkedin.com/shareArticle?mini=true&url=' + encodedUrl + '" class="article-share-linkedin" target="_blank" title="LinkedIn">',
+              '<i class="fab fa-linkedin-in"></i>',
+            '</a>',
+            '<a href="https://service.weibo.com/share/share.php?url=' + encodedUrl + '&title=' + encodeURIComponent(title) + '" class="article-share-weibo" target="_blank" title="Weibo">',
+              '<i class="fab fa-weibo"></i>',
+            '</a>',
           '</div>',
         '</div>'
       ].join('');
 
-      var box = $(html);
-
-      $('body').append(box);
+      $box = $(html);
+      $('body').append($box);
     }
 
-    $('.article-share-box.on').hide();
-
-    box.css({
-      top: offset.top + 25,
-      left: offset.left
+    // Position and show
+    $box.css({
+      top: offset.top + 30,
+      left: Math.min(offset.left, $(window).width() - $box.outerWidth() - 20)
     }).addClass('on');
-  }).on('click', '.article-share-box', function(e){
-    e.stopPropagation();
-  }).on('click', '.article-share-box-input', function(){
-    $(this).select();
-  }).on('click', '.article-share-box-link', function(e){
-    e.preventDefault();
-    e.stopPropagation();
 
-    window.open(this.href, 'article-share-box-window-' + Date.now(), 'width=500,height=450');
+    // Auto-select input
+    $box.find('.article-share-input').select();
   });
 
-  // Caption
-  $('.article-entry').each(function(i){
-    $(this).find('img').each(function(){
+  // Prevent closing when clicking inside share box
+  $('body').on('click', '.article-share-box', function(e) {
+    e.stopPropagation();
+  });
+
+  // Copy to clipboard functionality
+  $('body').on('click', '.article-share-input', function() {
+    this.select();
+    if (navigator.clipboard) {
+      navigator.clipboard.writeText(this.value).then(function() {
+        // Optional: Show toast notification
+      });
+    }
+  });
+
+  // ========================================
+  // Image Lightbox (Fancybox)
+  // ========================================
+  $('.article-entry').each(function(i) {
+    $(this).find('img').each(function() {
       if ($(this).parent().hasClass('fancybox') || $(this).parent().is('a')) return;
 
       var alt = this.alt;
-
       if (alt) $(this).after('<span class="caption">' + alt + '</span>');
 
-      $(this).wrap('<a href="' + this.src + '" data-fancybox=\"gallery\" data-caption="' + alt + '"></a>')
+      $(this).wrap('<a href="' + this.src + '" data-fancybox="gallery" data-caption="' + alt + '"></a>');
     });
 
-    $(this).find('.fancybox').each(function(){
+    $(this).find('.fancybox').each(function() {
       $(this).attr('rel', 'article' + i);
     });
   });
 
-  if ($.fancybox){
-    $('.fancybox').fancybox();
+  if ($.fancybox) {
+    $('.fancybox').fancybox({
+      loop: true,
+      animationEffect: 'fade',
+      transitionEffect: 'slide',
+      buttons: ['close', 'fullScreen', 'download'],
+      protect: true
+    });
   }
 
-  // Mobile nav
-  var $container = $('#container'),
-    isMobileNavAnim = false,
-    mobileNavAnimDuration = 200;
+  // ========================================
+  // Mobile Navigation
+  // ========================================
+  var $container = $('#container');
+  var $mobileNav = $('#mobile-nav');
 
-  var startMobileNavAnim = function(){
-    isMobileNavAnim = true;
-  };
-
-  var stopMobileNavAnim = function(){
-    setTimeout(function(){
-      isMobileNavAnim = false;
-    }, mobileNavAnimDuration);
-  }
-
-  $('#main-nav-toggle').on('click', function(){
-    if (isMobileNavAnim) return;
-
-    startMobileNavAnim();
+  $('#main-nav-toggle').on('click', function(e) {
+    e.preventDefault();
     $container.toggleClass('mobile-nav-on');
-    stopMobileNavAnim();
   });
 
-  $('#wrap').on('click', function(){
-    if (isMobileNavAnim || !$container.hasClass('mobile-nav-on')) return;
+  // Close mobile nav when clicking outside
+  $('#wrap').on('click', function(e) {
+    if ($container.hasClass('mobile-nav-on') && !$(e.target).closest('#main-nav-toggle').length) {
+      $container.removeClass('mobile-nav-on');
+    }
+  });
 
+  // Close mobile nav on link click
+  $('.mobile-nav-link').on('click', function() {
     $container.removeClass('mobile-nav-on');
   });
+
+  // Close mobile nav on Escape key
+  $(document).on('keydown', function(e) {
+    if (e.key === 'Escape' && $container.hasClass('mobile-nav-on')) {
+      $container.removeClass('mobile-nav-on');
+    }
+  });
+
+  // ========================================
+  // Smooth Scroll for Anchor Links
+  // ========================================
+  $('a[href^="#"]').on('click', function(e) {
+    var target = $(this.getAttribute('href'));
+    if (target.length) {
+      e.preventDefault();
+      $('html, body').animate({
+        scrollTop: target.offset().top - 80
+      }, 400);
+    }
+  });
+
+  // ========================================
+  // Scroll Effects
+  // ========================================
+  var $header = $('#header');
+  var lastScroll = 0;
+
+  $(window).on('scroll', function() {
+    var currentScroll = $(this).scrollTop();
+
+    // Add shadow to header on scroll
+    if (currentScroll > 10) {
+      $header.css('box-shadow', '0 1px 3px rgba(0,0,0,0.05)');
+    } else {
+      $header.css('box-shadow', 'none');
+    }
+
+    lastScroll = currentScroll;
+  });
+
+  // ========================================
+  // Article Card Hover Effect
+  // ========================================
+  $('.article-inner').on('mouseenter', function() {
+    $(this).addClass('hover');
+  }).on('mouseleave', function() {
+    $(this).removeClass('hover');
+  });
+
 })(jQuery);
